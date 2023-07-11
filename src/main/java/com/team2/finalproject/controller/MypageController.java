fc8a96ff8a9cf4aafa72f2f93cf4414564e3a0f2
Lim JunMook <dlawnsanr778@gmail.com>
2023-07-11 09:33:53 +0900

    로그인 되어있는 회원정보를 가지고 메인페이지로 이동

---
+++ src/main/java/com/team2/finalproject/controller/MypageController.java
@@ -0,0 +1,40 @@
+package com.team2.finalproject.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.team2.finalproject.dto.user.UserDto;
+import com.team2.finalproject.service.UserService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@AllArgsConstructor
+@RequestMapping("/mypage")
+public class MypageController {
+	
+	@Autowired
+	private UserService service;
+ 
+@PreAuthorize("isAuthenticated()")
+    //마이페이지 창으로 이동
+    @GetMapping
+    public void profile(Principal principal,Model model) {
+        
+        log.info("마이페이지 창으로 이동");
+        log.info("유저아이디: " + principal.getName());
+        String userid = principal.getName();
+        UserDto user = service.getUserByUserId(userid);
+        model.addAttribute("user", user);
+        
+        System.out.println(user);
+    }
+}
