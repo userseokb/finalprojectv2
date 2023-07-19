ce6688faf7eec212e75767957038dff35c2bd991
Choi Seion Gi <black1720@naver.com>
2023-07-19 15:44:41 +0900

    insertReview 완료

--- src/main/java/com/team2/finalproject/service/ReviewService.java
+++ src/main/java/com/team2/finalproject/service/ReviewService.java
@@ -1,28 +1,19 @@
 package com.team2.finalproject.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.team2.finalproject.dto.product.ProductDto;
 import com.team2.finalproject.dto.user.ReviewDto;
-import com.team2.finalproject.mapper.MainMapper;
 import com.team2.finalproject.mapper.ReviewMapper;
 
 @Service
 public class ReviewService {
 
-	@Autowired
-	ReviewMapper mapper;
-	MainMapper mainMapper;
-	
-	public ReviewDto insertReview(ReviewDto newReview){
+    @Autowired
+    ReviewMapper mapper;
 
-		return mapper.insertReview(newReview);
-	}
+    public void insertReview(ReviewDto newReview) {
+        mapper.insertReview(newReview);
+    }
 
-	public ProductDto getProductNameByProductCode(Integer productCode) {
-		return null;
-	}
 }
